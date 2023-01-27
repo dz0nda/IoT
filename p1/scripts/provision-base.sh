6c45203515023ce4537919d12f2ce1fb416aa845
dzonda <damien.zonda@lambda-coins.com>
2023-01-27 11:52:38 +0100

    p1

--- p1/scripts/provision-base.sh
+++ p1/scripts/provision-base.sh
@@ -1,24 +1,12 @@
 #! /bin/sh
 
+function setup_ssh() {
+    mv ./auth/id_rsa.pub            .ssh/id_rsa.pub
+    mv ./auth/id_rsa                .ssh/id_rsa
+    cat .ssh/id_rsa.pub >>    .ssh/authorized_keys
 
-function setup_root_ssh() {
-    sudo passwd root
-
-    mkdir -p /root/.ssh
-
-    mv ./id_rsa.pub /root/.ssh/id_rsa.pub
-    mv ./id_rsa     /root/.ssh/id_rsa
-    cat /root/.ssh/id_rsa.pub >>  /root/.ssh/authorized_keys
-
-    chmod 400  /root/.ssh/id_rsa*
-    chmod 400  /root/.ssh/authorized_keys
-    
-    chown root:root  /root/.ssh/id_rsa*
-    chown root:root  /root/.ssh/authorized_keys
+    chmod 400   .ssh/id_rsa*
+    chmod 400   .ssh/authorized_keys
 }
 
-function main() {
-    setup_root_ssh
-}
-
-main
+setup_ssh
