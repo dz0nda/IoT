b1f0ee6edbc8b0e74a6e0165b5b8b3dc751253e2
dzonda <damien.zonda@lambda-coins.com>
2023-03-21 19:59:46 +0100

    fix: p1

--- p1/scripts/provision-base.sh
+++ p1/scripts/provision-base.sh
@@ -1,18 +1,18 @@
 #! /bin/sh
 
-function setup_ssh() {
-    mv ./auth/id_rsa.pub            .ssh/id_rsa.pub
-    mv ./auth/id_rsa                .ssh/id_rsa
-    cat .ssh/id_rsa.pub >>    .ssh/authorized_keys
-    echo "PasswordAuthentication no" | tee -a /etc/ssh/sshd_config
-    service sshd restart
+function setup_ssh_keys() {
+    mv ./ssh/id_rsa.pub    .ssh/id_rsa.pub
+    mv ./ssh/id_rsa        .ssh/id_rsa
+    cat .ssh/id_rsa.pub     >> .ssh/authorized_keys
 
     chmod 400   .ssh/id_rsa*
     chmod 400   .ssh/authorized_keys
 }
 
-setup_ssh
+function setup_sshd() {
+    mv ./ssh/sshd_config    /etc/ssh/sshd_config
+    service sshd restart
+}
 
-echo "alias k='kubectl'" | tee -a "/home/vagrant/.profile"
-chown vagrant:vagrant /home/vagrant/.profile
-chmod 644 /home/vagrant/.profile
+setup_ssh
+setup_sshd
--- p1/scripts/provision-worker.sh
+++ p1/scripts/provision-worker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JOIN_TOKEN=$(ssh -o StrictHostKeyChecking=no -i /home/vagrant/.ssh/id_rsa vagrant@192.168.42.110 "sudo cat /var/lib/rancher/k3s/server/node-token")
+JOIN_TOKEN=$(ssh -o StrictHostKeyChecking=no -i /home/vagrant/.ssh/id_rsa vagrant@192.168.42.110 "sudo cat /var/lib/rancher/k3s/server/token")
 
 export K3S_URL=https://192.168.42.110:6443
 export K3S_TOKEN=$JOIN_TOKEN
