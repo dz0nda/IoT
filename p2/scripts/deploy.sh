b345294c199fd25d0164c1f4c3d141f9d7df126a
guyb27 <gmadec.student@protonmail.com>
2023-03-21 08:10:05 +0100

    p2 en cours

--- p2/scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-kubectl apply -f manifests/app1/configmap.yml
-kubectl apply -f manifests/app1/deployment.yml
-kubectl apply -f manifests/app1/service.yml
-kubectl apply -f manifests/app1/ingress.yml
